33af88bb8e65c55d7154fbd7f59f3ff63b5a8355
sheltonmac2000 <shelton.macamo@icloud.com>
2023-09-23 15:21:04 -0600

    Minor changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What percentage was your grade?");
+        string userInput = Console.ReadLine();
+        int inputInteger = Convert.ToInt32(userInput);
+        Console.WriteLine($"This is the number that you printed out: {inputInteger}");
+
+        while (inputInteger != 0)
+            if (inputInteger >= 90)
+            {
+                Console.WriteLine($"Congratulations, you got A.");
+            }
+            else if (inputInteger >= 80)
+            {
+                Console.WriteLine($"Congratulations, you got a B.");
+            }
+            else if (inputInteger >= 70)
+            {
+                Console.WriteLine($"Congratulations, you got a C.");
+            }
+            else if (inputInteger >= 60)
+            {
+                Console.WriteLine($"Congratulations, you got a D.");
+            }
+            else if (inputInteger < 60)
+            {
+                Console.WriteLine($"You got an F. You can surely do better.");
+            }
+        else 
+            { Console.WriteLine("You quit the program.") };
+        
     }
 }
+
+
+
+
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string inputInfo;
+
+        do {
+            Console.WriteLine("Do you want to login?");
+            string inputInfo = Console.ReadLine();
+            Console.WriteLine("What is your name ?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Welcom back {userName}");
+        } while (inputInfo == "yes" );
+
     }
 }
